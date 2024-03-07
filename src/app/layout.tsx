2cd2d1805e36a7f0955aeefc5b45ccac5da8a6a7
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2024-03-07 23:58:17 +0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import { Montserrat } from 'next/font/google';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import './globals.css';
 
 const montserrat = Montserrat({
@@ -26,24 +25,22 @@ export default function RootLayout({
 	return (
     <ClerkProvider>
       <html lang="en">
-        <link
-          href="https://unpkg.com/aos@2.3.1/dist/aos.css"
-          rel="stylesheet"
-        />
+        
         <body className={montserrat.className}>
           <Navbar />
           <ToastContainer />
           {children}
         </body>
-        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
         <script
           src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
           integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+          crossorigin="anonymous"
         ></script>
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
           integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+          crossorigin="anonymous"
         ></script>
       </html>
     </ClerkProvider>
