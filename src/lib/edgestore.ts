a6499087c463088171366c1e39e4f5bdfd969276
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2024-03-08 17:25:24 +0500

    lesson 1

--- src/lib/edgestore.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { type EdgeStoreRouter } from "../app/api/edgestore/[...edgestore]/page";
-import { createEdgeStoreProvider } from "@edgestore/react";
-
-const { EdgeStoreProvider, useEdgeStore } =
-  createEdgeStoreProvider<EdgeStoreRouter>();
-
-export { EdgeStoreProvider, useEdgeStore };
